aac7874be25dbddaa95bbda2348e17fb8bef35cb
Agustinus Nathaniel <agustinusnathaniel228@gmail.com>
2021-10-07 20:27:55 +0700

    feat: custom swiper nav

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,15 +1,50 @@
-import { Grid } from "@chakra-ui/react";
+import SwiperCore, { Navigation } from "swiper";
+import { Box, Button, Grid, Stack } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Ref, useEffect, useRef, useState } from "react";
 
-import CTASection from "./components/CTASection";
-import SomeImage from "./components/SomeImage";
-import SomeText from "./components/SomeText";
+import "swiper/css";
+
+SwiperCore.use([Navigation]);
+
+const images = ["-G3rw6Y02D0", "JFeOy62yjXk", "GPPAjJicemU"];
+
+const useSwiperRef = <T extends HTMLElement>(): [
+  T | null | undefined,
+  Ref<T>
+] => {
+  const [wrapper, setWrapper] = useState<T | null>(null);
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      setWrapper(ref.current);
+    }
+  }, []);
+
+  return [wrapper, ref];
+};
 
 const Home = () => {
+  const [nextEl, nextElRef] = useSwiperRef<HTMLButtonElement>();
+  const [prevEl, prevElRef] = useSwiperRef<HTMLButtonElement>();
+
   return (
     <Grid gap={4}>
-      <SomeText />
-      <SomeImage />
-      <CTASection />
+      <Box maxWidth="800">
+        <Swiper navigation={{ prevEl, nextEl }} slidesPerView={1}>
+          {images.map((img) => (
+            <SwiperSlide key={img}>
+              <img src={`https://source.unsplash.com/${img}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+
+      <Stack direction="row" spacing={4}>
+        <Button ref={prevElRef}>Prev</Button>
+        <Button ref={nextElRef}>Next</Button>
+      </Stack>
     </Grid>
   );
 };
